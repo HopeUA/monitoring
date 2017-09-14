a490d7907546da412e3c32e869a69e9d3db65775
ZEVEKE <zeveke.developing@gmail.com>
2017-09-14 16:21:21 +0300

    feat($components) NotFound
    Add NotFound component

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Styles from './styles.scss';
+
+/* eslint-disable react/prefer-stateless-function */
+const NotFound = () => (
+    <section className = { Styles.notFoundComponent }>
+        <div>Not found</div>
+    </section>
+);
+
+export default NotFound;
